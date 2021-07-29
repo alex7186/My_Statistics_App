fe0b284070d902c9966aab467a66dd83b3eb25cf
alex7186 <sasha2649058@mail.ru>
2021-07-30 00:52:02 +0300

    Update server_aiohttp.py

--- server_aiohttp.py
+++ server_aiohttp.py
@@ -32,21 +32,18 @@ def arr_1_server_answer(
         result[0]['content'] = text_arr[0]
 
     if param_make_hist:
-       # result[1]['content'] = _decode_buff_to_bytes(StatAnalysis_1_arr_make_hist(sorted(analysis.arr)))
        result[1]['content'] = _decode_buff_to_bytes(StatAnalysis_1_arr_make_hist(sorted(arr)))
     
     if make_text:
         result[2]['content'] = text_arr[1]
     
     if param_make_boxplot:
-       # result[3]['content'] = _decode_buff_to_bytes(StatAnalysis_1_arr_make_boxplot(sorted(analysis.arr)))
        result[3]['content'] = _decode_buff_to_bytes(StatAnalysis_1_arr_make_boxplot(sorted(arr)))
     
     if make_text:
         result[4]['content'] = text_arr[2]
 
     if true_value > -1:
-       # result[5]['content'] = _decode_buff_to_bytes(StatAnalysis_1_true_plot(analysis.arr, true_value))
        result[5]['content'] = _decode_buff_to_bytes(StatAnalysis_1_true_plot(arr, true_value))
 
     return result
@@ -80,37 +77,29 @@ async def server(request):
 
     print(str(datetime.now())[:-7], ' - new request')
 
-    # request_str = await request.read()
-    # print(request_str)
-    # try:
-    #     data = loads(request_str)        
-    # except JSONDecodeError:
-    #     print(str(datetime.now())[:-7], ' - input JSONDecodeError')
-    #     return web.Response(text='input JSONDecodeError')
+    request_str = await request.read()
+    print(request_str)
+    try:
+        data = loads(request_str)        
+    except JSONDecodeError:
+        print(str(datetime.now())[:-7], ' - input JSONDecodeError')
+        return web.Response(text='input JSONDecodeError')
 
-    server_return = arr_1_server_answer(
-        [42.0, 42.5, 41.9, 41.5, 41.8],
-        -100,
-        -100,
-        1,
-        0,
-        0,)
+    if data['req_type'] == '1_arr':
 
-    # if data['req_type'] == '1_arr':
+        server_return = arr_1_server_answer(
+            data['arr_data'], 
+            data['sr_met'], 
+            data['true_value'], 
+            data['make_text'], 
+            data['make_hist'], 
+            data['make_boxplot']
+            )
 
-    #     server_return = arr_1_server_answer(
-    #         data['arr_data'], 
-    #         data['sr_met'], 
-    #         data['true_value'], 
-    #         data['make_text'], 
-    #         data['make_hist'], 
-    #         data['make_boxplot']
-    #         )
-
-    # elif data['req_type'] == '2_arr':
-    #     server_return = arr_2_server_answer(
-    #         data['arr1'], data['arr2'], data['make_text'], data['make_boxplot']
-    #         )
+    elif data['req_type'] == '2_arr':
+        server_return = arr_2_server_answer(
+            data['arr1'], data['arr2'], data['make_text'], data['make_boxplot']
+            )
 
     return web.Response(text=dumps(server_return))
     
